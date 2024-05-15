d45c2654b8bff80b9794d069c237d8ba74776b7e
Ryzex911 <ryzexgamer1@gmail.com>
2024-05-15 10:50:16 +0200

    first commit

---
+++ HTML/login.php
@@ -0,0 +1,21 @@
+<?php
+require_once "navbar.php";
+require_once "connection.php";
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>login</title>
+</head>
+<body class="login-body">
+
+</body>
+</html>
---
+++ HTML/navbar.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="styles.css">
+<div class="nav-parent">
+<nav role="navigation">
+    <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+            <a href="index.php"><li>Home</li></a>
+            <a href="#"><li>About</li></a>
+            <a href="#"><li>Reizen</li></a>
+            <a href="login.php"><li>Login</li></a>
+        </ul>
+    </div>
+</nav>
+</div>
