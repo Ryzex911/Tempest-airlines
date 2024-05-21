dd81f6499fa1d13c70c7963f5ea1ed5b1a99fc4d
Ryzex911 <ryzexgamer1@gmail.com>
2024-05-21 16:30:38 +0200

    x

--- HTML/login.php
+++ HTML/login.php
@@ -3,7 +3,6 @@ require_once "navbar.php";
 require_once "connection.php";
 ?>
 
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -12,14 +11,22 @@ require_once "connection.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <title>login</title>
 </head>
 <body class="login-body">
-        <div class="login-parent">
-            <div class="login-form">
 
-            </div>
-        </div>
+<div class="login-parent">
+    <div class="login-form">
+        <h1 class="first-text">Get the full experience</h1>
+        <p class="second-text">Track prices, make trip planning easier <br> and enjoy faster booking.</p>
+        <ul class="login-links">
+            <li><a class="apple-logo" href=""><img class="apple" src="pics/Apple%20Logo.png" alt="apple-logo">Continue with Apple</a></li>
+            <li><a class="facebook-logo" href=""><img class="facebook" src="pics/Facebook.png" alt="facebook-logo">Continue with Facebook</a></li>
+            <li><a class="google-logo" href=""><img class="google" src="pics/Google.png" alt="google-logo">Continue with Google</a></li>
+            <li><a class="email-logo" href=""><img class="email" src="pics/Email.png" alt="email-logo">Continue with Email</a></li>
+        </ul>
+    </div>
+</div>
 </body>
 </html>
