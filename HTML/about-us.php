26227cefe99e2761e2a387b701daf6b5bea9848e
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2024-05-21 11:25:09 +0200

    update about
    br weg
    en text verandert

--- HTML/about-us.php
+++ HTML/about-us.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Home</title>
+    <title>About us</title>
 </head>
 <body>
 <div class="main">
@@ -16,7 +16,7 @@
         </div>
         <div class="text">
             Bij Prijsvrij Vakanties gaan we tot het uiterste <br>om onze klanten de beste reiservaring te <br> bieden, zonder de prijs uit het oog te <br>verliezen. Met een indrukwekkende Trustpilot-<br>score van 8,6, beloven we transparantie en <br>kwaliteit in al onze diensten. Geen verborgen <br>kosten, alleen de beste deals.
-        </div><br>
+        </div>
         <div class="text">
             Onze toewijding aan klanttevredenheid heeft <br>
             een sterke online gemeenschap gecreëerd, <br>
@@ -25,7 +25,7 @@
             trots op het vertrouwen dat onze klanten in <br>
             ons stellen en de liefde die ze tonen voor onze <br>
             service. <br>
-        </div><br>
+        </div>
         <div class="text">
             Al meer dan een decennium lang is Prijsvrij <br>
             Vakanties dé plek voor de beste <br>
