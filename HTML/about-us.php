1a249ca1248d72bcf57a81576988d15c4754ac37
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2024-05-21 11:13:16 +0200

    x

--- HTML/about-us.php
+++ HTML/about-us.php
@@ -1,2 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+<div class="main">
+    <div class="about-us-text">
+        <div class="head">
+            About us
+        </div>
+        <div class="text">
+            Bij Prijsvrij Vakanties gaan we tot het uiterste <br>om onze klanten de beste reiservaring te <br> bieden, zonder de prijs uit het oog te <br>verliezen. Met een indrukwekkende Trustpilot-<br>score van 8,6, beloven we transparantie en <br>kwaliteit in al onze diensten. Geen verborgen <br>kosten, alleen de beste deals.
+        </div><br>
+        <div class="text">
+            Onze toewijding aan klanttevredenheid heeft <br>
+            een sterke online gemeenschap gecreëerd, <br>
+            met meer dan 215.000 fans op Facebook en <br>
+            meer dan 17.500 volgers op Instagram. We zijn <br>
+            trots op het vertrouwen dat onze klanten in <br>
+            ons stellen en de liefde die ze tonen voor onze <br>
+            service. <br>
+        </div><br>
+        <div class="text">
+            Al meer dan een decennium lang is Prijsvrij <br>
+            Vakanties dé plek voor de beste <br>
+            vakantieaanbiedingen. Met een zorgvuldig <br>
+            geselecteerd assortiment van reizen, van <br>
+            ontspannende zonvakanties tot opwindende <br>
+            stedentrips en avontuurlijke reizen over de <br>
+            hele wereld, bieden we voor elk wat wils. Onze <br>
+            expertise en toewijding staan garant voor een <br>
+            vakantie die aan al je verwachtingen voldoet. <br>
+        </div><br>
+    </div>
+</div>
+</body>
+</html>
 
-<?php
