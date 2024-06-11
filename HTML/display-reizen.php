52abd9f7e7d2cbab70bfb6eee8519a4f318fa360
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2024-06-11 11:30:59 +0200

    x

---
+++ HTML/display-reizen.php
@@ -0,0 +1,18 @@
+<?php
+require_once "navbar.php";
+require_once "connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clouds & Chill Flying Agency</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="main">
+<div class="booking">Book you journey</div>
+</div>
+</body>
