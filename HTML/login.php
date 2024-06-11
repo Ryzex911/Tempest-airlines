2941e5e381b7d88348a2726b18b3f0437de4f945
Ryzex911 <ryzexgamer1@gmail.com>
2024-06-11 11:35:09 +0200

    x

--- HTML/login.php
+++ HTML/login.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->execute([$email]);
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        $user = $stmt->fetch();
 
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
--- HTML/navbar.php
+++ HTML/navbar.php
@@ -1,3 +1,4 @@
+
 <link rel="stylesheet" href="css/styles.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -11,7 +12,10 @@
         <li><a href="about-us.php">About</a></li>
         <li><a href="reizen.php">Reizen</a></li>
         <li><a href="contact.php">Contact</a></li>
-        <li><a href="email-login-form.php">Login</a></li>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php else: ?>
+            <li><a href="email-login-form.php">Login</a></li>
+        <?php endif; ?>
     </ul>
 </div>
-
