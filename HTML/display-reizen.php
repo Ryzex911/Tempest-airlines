f202f37bd8bc3647f7c5ace18790095e0a2bc19d
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2024-06-12 12:03:41 +0200

    Update display-reizen.php

--- HTML/display-reizen.php
+++ HTML/display-reizen.php
@@ -1,6 +1,9 @@
 <?php
 require_once "navbar.php";
 require_once "connection.php";
+
+$sql = "SELECT img, besteming, prijs FROM reizen";
+$result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,19 +17,31 @@ require_once "connection.php";
 <div class="main">
 <div class="booking">Book you journey</div>
     <div class="toprow-reizen">
-        <div class="reis-boek">
-            <div class="besteming">Spanje</div>
-            <div class="flex-direction">
-                <div class="gap">
-                    <div class="text-book">voor maar </div>
-                    <div class="prijs">666 </div>
-                </div>
-                <div class="gap">
-                    <div class="prijs-sign">,-</div>
-                    <div class="text-book"> p.p.</div>
-                </div>
-            </div>
-        </div>
+        <?php
+        function displayReis($Reis) {
+        echo "<div class='reis-boek'>";
+        echo "<div class='besteming'>" . $Reis['locatie'] . "</div>";
+            echo "<div class='flex-direction'>";
+                echo "<div class='gap'>";
+                    echo "<div class='text-book'>voor maar ";
+                    echo "<div class='prijs'>". $Reis['prijs'] . ",-</div>";
+                echo "</div>";
+                echo "<div class='gap'>";
+                    echo "<div class='text-book'> p.p.";
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+        }
+        ?>
+
+            echo "<div class='gerecht'>";
+            echo "<div class='font title'>" . $Reis['titel'] . "</div>";
+            echo "<div class='beschrijving text-cat margin-top-'>" . $Reis['beschrijving'] . "</div>";
+            echo "<div class='bestel margin-top-'>Price: $" . $Reis['prijs'] . "</div>";
+            if (!empty($Reis['image'])) {
+                echo "<img class='img-order border-radius-twee' width='140' style='margin-top: 12px;' src='pics/" . $Reis['image'] . "' />";
+            }
+
 
     </div>
     <div class="bottomrow-reizen">
